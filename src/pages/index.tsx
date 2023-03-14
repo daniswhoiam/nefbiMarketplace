0764832612735bdd7f9acab47550aec1194caa5d
daniswhoiam <daniil.belazovschi@gmail.com>
2023-03-14 14:58:16 +0200

    startpage tutorial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,35 +33,39 @@ const Index = (props: any) => {
           className="z-5 absolute left-[42%] -bottom-[27%] fill-light-sea-green-dark"
         />
       </div>
-      <div className="tutorial relative w-screen">
-        <div className="steps container mx-auto grid max-w-screen-xl grid-cols-2">
-          <h2 className="col-span-2">Wie nefbi funktioniert</h2>
-          <p className="col-span-2 pr-[70%]">
+      <div className="relative mt-20 w-screen">
+        <div className="container mx-auto max-w-screen-xl">
+          <h2 className=" text-4xl">Wie nefbi funktioniert</h2>
+          <p className=" pr-[60%] text-lg font-medium">
             nefbi ermöglicht das Suchen und Finden von Materialien. Im Suchfeld
             kannst du Schlagworte eingeben. Zusätzlich ermöglicht die
             Filterfunktion, Ergebnisse besser einzugrenzen. <br></br> Wie die
             Filterfunktion funktioniert, erklären wir in drei Schritten:
           </p>
-          <StepOne className="col-span-1" />
-          <div className="step-one col-span-1">
-            <h3>Auswahl des Arbeitsbereiches</h3>
-            <p>
+        </div>
+      </div>
+      <div className="tutorial relative mt-9 w-screen">
+        <div className="steps container mx-auto grid max-w-screen-xl grid-cols-2 gap-16">
+          <StepOne className="col-span-1 self-center justify-self-end" />
+          <div className="step-one col-span-1 self-center justify-self-start pr-48">
+            <h3 className="text-3xl">Auswahl des Arbeitsbereiches</h3>
+            <p className="text-lg">
               Wähle deinen Arbeitsbereich aus: Kindertagesstätte,
               Kindertagespflege oder Hort/Ganztagsgrundschule.
             </p>
           </div>
-          <div className="step-two col-span-1">
-            <h3>Auswahl des gewünschten Formates</h3>
-            <p>
+          <div className="step-two col-span-1 self-center justify-self-end pl-48">
+            <h3 className="text-3xl">Auswahl des gewünschten Formates</h3>
+            <p className="text-lg">
               Treffe eine Auswahl für das gewünschte Format: Online-Kurs,
               Fachtext, Podcast, Video, Praxisimpuls und Aus der Praxis
             </p>
           </div>
-          <StepTwo className="col-span-1" />
-          <StepThree className="col-span-1" />
-          <div className="step-three col-span-1">
-            <h3>Themenauswahl</h3>
-            <p>
+          <StepTwo className="col-span-1 self-center justify-self-start" />
+          <StepThree className="col-span-1 self-center justify-self-end" />
+          <div className="step-three col-span-1 self-center justify-self-start pr-48">
+            <h3 className="text-3xl">Themenauswahl</h3>
+            <p className="text-lg">
               Wähle eine Vielzahl von Themen aus unseren drei Kategorien aus:
               Fokusthemen, Pädagogische Grundlagen und Mensch, Organisation und
               Management.
@@ -70,7 +74,7 @@ const Index = (props: any) => {
         </div>
       </div>
       <div className="action-banner relative w-screen">
-        <div className="container mx-auto flex flex-col max-w-screen-xl">
+        <div className="container mx-auto flex max-w-screen-xl flex-col">
           <h2>Jetzt loslegen!</h2>
           <button className="btn-primary btn">nefbi Suche</button>
         </div>
