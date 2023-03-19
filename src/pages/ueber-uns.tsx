732482ce14da0bae6b40a418e02c0c0ee0c7d81d
daniswhoiam <daniil.belazovschi@gmail.com>
2023-03-19 15:34:30 +0200

    v1 mobile about us

--- src/pages/ueber-uns.tsx
+++ src/pages/ueber-uns.tsx
@@ -2,14 +2,21 @@ import React from "react"
 
 import SideHexagons from "../assets/images/side_hexagons.svg"
 import HexagonBottomCloud from "../assets/images/hexagon_bottom_cloud.svg"
+import Qualitaet from "../assets/images/qualitaet.svg"
+import Zugang from "../assets/images/zugang.svg"
+import Bildung from "../assets/images/bildung.svg"
+import Innovation from "../assets/images/innovation.svg"
 
 const UeberUns = () => {
   return (
-    <div>
-      <SideHexagons />
-      <h1>Über uns</h1>
-      <div>
-        <h2>nefbi - Netzwerk Frühe Bildung</h2>
+    <div className="mx-4 pb-36">
+      <SideHexagons
+        viewBox="-240 -10 700 700"
+        className="absolute -top-4 -right-24 w-72"
+      />
+      <h1 className="mt-24">Über uns</h1>
+      <div className="mt-36">
+        <h2 className="text-xl">nefbi - Netzwerk Frühe Bildung</h2>
         <p>
           nefbi ist ein digitaler Raum für alle, die in der Frühen Bildung
           arbeiten: Für pädagogische Fachkräfte in Ausbildung, für
@@ -22,57 +29,73 @@ const UeberUns = () => {
           Austausch und Vernetzung.
         </p>
       </div>
-      <div>
-        <h2>Was uns wichtig ist</h2>
-        <div>
-          <div>
-            <img src="" alt="" />
-            <p>Qualität</p>
-            <p>Unsere Materialien entsprechen eigens entwickelten Kriterien</p>
+      <div className="mt-24">
+        <h2 className="text-xl">Was uns wichtig ist</h2>
+        <div className="grid grid-cols-1 gap-y-16">
+          <div className="flex flex-col items-center text-center">
+            <Qualitaet viewBox="0 0 150 130" className="mb-4 w-56" />
+            <p className="font-semibold">Qualität</p>
+            <p className="px-12">
+              Unsere Materialien entsprechen eigens entwickelten Kriterien
+            </p>
           </div>
-          <div>
-            <img src="" alt="" />
-            <p>Zugang</p>
-            <p>Die Materialien sind leicht zu finden und direkt nutzbar</p>
+          <div className="flex flex-col items-center text-center">
+            <Zugang viewBox="0 0 150 150" className="mb-4 w-56" />
+            <p className="font-semibold">Zugang</p>
+            <p className="px-12">
+              Die Materialien sind leicht zu finden und direkt nutzbar
+            </p>
           </div>
-          <div>
-            <img src="" alt="" />
-            <p>Bildung</p>
-            <p>
+          <div className="flex flex-col items-center text-center">
+            <Bildung viewBox="0 0 160 150" className="mb-4 w-56" />
+            <p className="font-semibold">Bildung</p>
+            <p className="px-12">
               Unsere Materialen regen zur Weiterentwicklung der beruflichen
               Rolle an
             </p>
           </div>
-          <div>
-            <img src="" alt="" />
-            <p>Innovation</p>
-            <p>
+          <div className="flex flex-col items-center text-center">
+            <Innovation viewBox="0 0 150 170" className="mb-4 w-56" />
+            <p className="font-semibold">Innovation</p>
+            <p className="px-12">
               nefbi wird stetig weiterentwickelt und um neue Funktionen ergänzt
             </p>
           </div>
         </div>
       </div>
-      <div>
-        <h2>Team</h2>
-        <div>
-          <div>
-            <img src="" alt="" />
+      <div className="mt-24">
+        <h2 className="mb-16">Team</h2>
+        <div className="grid grid-cols-1 gap-y-12">
+          <div className="flex flex-col items-center">
+            <img
+              src="/daniil_rs.jpeg"
+              alt="Bild von Daniil Belazovschi"
+              className="mb-4 w-48"
+            />
             <p>Daniil Belazovschi</p>
-            <p>Gründer</p>
+            <p className="text-jet-dark-grey">Gründer</p>
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className="flex flex-col items-center">
+            <img
+              src="/natascha_rs.jpg"
+              alt="Bild von Natascha Berger"
+              className="mb-4 w-48"
+            />
             <p>Natascha Berger</p>
-            <p>Gründerin</p>
+            <p className="text-jet-dark-grey">Gründerin</p>
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className="flex flex-col items-center">
+            <img
+              src="/luecy_rs.jpg"
+              alt="Bild von Katarina Lücy Fuchs"
+              className="mb-4 w-48"
+            />
             <p>Katarina Lücy Fuchs</p>
-            <p>Gründerin</p>
+            <p className="text-jet-dark-grey">Gründerin</p>
           </div>
         </div>
       </div>
-      <HexagonBottomCloud />
+      <HexagonBottomCloud viewBox="-150 -20 1700 800" className="absolute bottom-0 -left-[20vw] w-[140vw]" />
     </div>
   )
 }
