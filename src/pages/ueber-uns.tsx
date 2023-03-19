f5f941af7ecbd6af5bc060fb4788acb7a4905163
daniswhoiam <daniil.belazovschi@gmail.com>
2023-03-19 14:48:10 +0200

    html skeleton about us

--- src/pages/ueber-uns.tsx
+++ src/pages/ueber-uns.tsx
@@ -1,7 +1,80 @@
 import React from "react"
 
+import SideHexagons from "../assets/images/side_hexagons.svg"
+import HexagonBottomCloud from "../assets/images/hexagon_bottom_cloud.svg"
+
 const UeberUns = () => {
-  return <>Über uns</>
+  return (
+    <div>
+      <SideHexagons />
+      <h1>Über uns</h1>
+      <div>
+        <h2>nefbi - Netzwerk Frühe Bildung</h2>
+        <p>
+          nefbi ist ein digitaler Raum für alle, die in der Frühen Bildung
+          arbeiten: Für pädagogische Fachkräfte in Ausbildung, für
+          Kindertagespflegepersonen, für Mitarbeitende in
+          Kindertageseinrichtungen, Ganztagsgrundschulen und im Hort. nefbi ist
+          ein Raum zum Suchen und Finden, zum Informieren und Weiterbilden. Hier
+          findest du vielfältige Materialien zu relevanten pädagogischen Themen.{" "}
+          <br />
+          Zukünftig bietet nefbi neben der Suchfunktion auch die Möglichkeit für
+          Austausch und Vernetzung.
+        </p>
+      </div>
+      <div>
+        <h2>Was uns wichtig ist</h2>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <p>Qualität</p>
+            <p>Unsere Materialien entsprechen eigens entwickelten Kriterien</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Zugang</p>
+            <p>Die Materialien sind leicht zu finden und direkt nutzbar</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Bildung</p>
+            <p>
+              Unsere Materialen regen zur Weiterentwicklung der beruflichen
+              Rolle an
+            </p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Innovation</p>
+            <p>
+              nefbi wird stetig weiterentwickelt und um neue Funktionen ergänzt
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>Team</h2>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <p>Daniil Belazovschi</p>
+            <p>Gründer</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Natascha Berger</p>
+            <p>Gründerin</p>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <p>Katarina Lücy Fuchs</p>
+            <p>Gründerin</p>
+          </div>
+        </div>
+      </div>
+      <HexagonBottomCloud />
+    </div>
+  )
 }
 
 export default UeberUns
