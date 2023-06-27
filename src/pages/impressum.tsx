2bf0c29efe1fe8f6364e20e5ae82928455f5644c
daniswhoiam <daniil.belazovschi@gmail.com>
2023-06-27 16:04:01 +0200

    new data

--- src/pages/impressum.tsx
+++ src/pages/impressum.tsx
@@ -1,7 +1,50 @@
 import React from 'react';
 
 const Impressum = () => {
-  return <>Impressum</>;
+  return (
+    <>
+      <h1>Impressum</h1>
+
+      <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+      <p>
+        Daniil Belazovschi, Natascha Berger und Katarina L&uuml;cy Fuchs - nefbi
+        GbR
+        <br />
+        Innere Kanalstr. 218
+        <br />
+        50670 K&ouml;ln
+      </p>
+
+      <p>
+        <strong>Vertreten durch:</strong>
+        <br />
+        Daniil Belazovschi
+        <br />
+        Natascha Berger
+        <br />
+        Katarina L&uuml;cy Fuchs
+      </p>
+
+      <h2>Kontakt</h2>
+      <p>
+        Telefon: &#91;Telefonnummer&#93;
+        <br />
+        E-Mail: admin@nefbi.de
+      </p>
+
+      <h2>
+        Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+      </h2>
+      <p>
+        Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+        vor einer Verbraucherschlichtungsstelle teilzunehmen.
+      </p>
+
+      <p>
+        Quelle: <a href="https://www.e-recht24.de">eRecht24</a>
+      </p>
+    </>
+  );
 };
 
 export default Impressum;
